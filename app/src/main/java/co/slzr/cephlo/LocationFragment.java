3c624b00c8cb3565959caf3dfbb3ff09505f6386
Alexander Selzer <aselzer3@gmail.com>
2015-09-07 10:59:20 +0200

    map stuff

--- app/src/main/java/co/slzr/cephlo/LocationFragment.java
+++ app/src/main/java/co/slzr/cephlo/LocationFragment.java
@@ -8,6 +8,11 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import org.osmdroid.api.IMapController;
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
+import org.osmdroid.util.GeoPoint;
+import org.osmdroid.views.MapView;
+
 /**
  * Created by as on 01/09/15.
  */
@@ -20,5 +25,20 @@ public class LocationFragment extends Fragment {
                 R.layout.fragment_location, container, false);
 
         return rootView;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        MapView map = (MapView) getActivity().findViewById(R.id.map);
+        map.setTileSource(TileSourceFactory.MAPNIK);
+        map.setBuiltInZoomControls(true);
+        map.setMultiTouchControls(true);
+
+        IMapController mapController = map.getController();
+        mapController.setZoom(9);
+        GeoPoint startPoint = new GeoPoint(48.3210993, 16.2090994);
+        mapController.setCenter(startPoint);
     }
 }
